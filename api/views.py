29b97a34c00ef9c626e10945662427e0f4ef1404
Erik Garces <egarces@GIMPROMED.local>
2024-07-24 15:26:36 -0500

    api -add email log

--- api/views.py
+++ api/views.py
@@ -2,6 +2,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+# Models
+from datos.models import EmailApiLog
+
 # Email
 from django.core.mail import EmailMultiAlternatives
 
@@ -37,16 +40,29 @@ def reg_sanitario_correo_alerta_list(request):
             subject    = 'Alerta - Documentos próximos a caducar.',
             from_email = settings.EMAIL_HOST_USER,
             to         = ['ronaldm@gimpromed.com','pespinosa@gimpromed.com','ncaisapanta@gimpromed.com'],
-            # to         = ['egarces@gimpromed.com'],
             body       = plain_message,
         )
         
         email.attach_alternative(html_message, 'text/html')
         email.send()
         
+        EmailApiLog.objects.create(
+            nombre='Email proximos a vencer en 180 días',
+            description = ' - '.join([str(i) for i in rs_list]),
+            error = 'N/A',
+            enviado = True
+        )
+        
         return Response(status=200)
     
-    except:
+    except Exception as e:
+        
+        EmailApiLog.objects.create(
+            nombre='Email proximos a vencer en 180 días',
+            description = ' - '.join([str(i) for i in rs_list]),
+            error = str(e),
+            enviado = True
+        )
         return Response(status=500)
 
 
@@ -61,7 +77,7 @@ def reg_sanitario_correo_alerta_dias(request):
         # Avisos
         a1=120
         a2=100
-        a3=90 #;a4=9
+        a3=90 
         a4=40
         
         # lista de correos 
@@ -87,6 +103,8 @@ def reg_sanitario_correo_alerta_dias(request):
                 email.attach_alternative(html_message, 'text/html')
                 email.send()
                 
+                text_log = f'aviso: {a1} dias'
+                
             elif i.dias_caducar == a2:  
                 # 2do Aviso
                 rs_list = [i for i in tabla_query if i.dias_caducar==a2]
@@ -104,6 +122,8 @@ def reg_sanitario_correo_alerta_dias(request):
                 
                 email.attach_alternative(html_message, 'text/html')
                 email.send()
+                
+                text_log = f'aviso: {a1} dias'
                 
             elif i.dias_caducar == a3:  
                 # 3er Aviso
@@ -123,6 +143,8 @@ def reg_sanitario_correo_alerta_dias(request):
                 email.attach_alternative(html_message, 'text/html')
                 email.send()
                 
+                text_log = f'aviso: {a3} dias'
+                
             elif i.dias_caducar == a4: 
                 # 4to Aviso
                 rs_list = [i for i in tabla_query if i.dias_caducar==a4]
@@ -134,7 +156,6 @@ def reg_sanitario_correo_alerta_dias(request):
                 email = EmailMultiAlternatives(
                     subject    = f'4to Aviso Próximo a Caducar - {i.registro} - {i.marca} - ({i.dias_caducar} días)',
                     from_email = settings.EMAIL_HOST_USER,
-                    # to         = lista_correos,
                     to         = lista_correos + ['ronaldm@gimpromed.com'],
                     body       = plain_message,
                 )
@@ -142,7 +163,26 @@ def reg_sanitario_correo_alerta_dias(request):
                 email.attach_alternative(html_message, 'text/html')
                 email.send()
                 
+                text_log = f'aviso: {a4} dias'
+                
+                # TABLA SAVE SEND
+            
+        EmailApiLog.objects.create(
+            nombre=f'Email {text_log}',
+            description = ' - '.join([str(i) for i in rs_list]),
+            error = 'N/A',
+            enviado = True
+        )
+                
         return Response(status=200)
     
-    except:
+    except Exception as e:
+        
+        EmailApiLog.objects.create(
+            nombre=f'Email {text_log}',
+            description = ' - '.join([str(i) for i in rs_list]),
+            error = str(e),
+            enviado = False
+        )
+                
         return Response(status=500)
