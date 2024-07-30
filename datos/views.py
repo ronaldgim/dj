8be0ed472594d066503aa151b0a1803f30853285
Erik Garces <egarces@GIMPROMED.local>
2024-07-30 10:39:02 -0500

    ssh fillna

--- datos/views.py
+++ datos/views.py
@@ -505,10 +505,10 @@ def etiquetado_fun():
     stock = stock.sort_values(by=['GROUP_CODE'], ascending=[True])
 
     ### STOCK SEGURIDAD MENSUAL
-    stock_mensual = frecuancia_ventas() ;print(stock_mensual)
+    stock_mensual = frecuancia_ventas().fillna(0) ;print(stock_mensual)
     stock_mensual['Mensual'] = (stock_mensual['ANUAL'] / 12).round(0)
     #stock_mensual['Mensual'] = (stock_mensual['anual'] / 12).round(0)
-    #stock_mensual['Cat'] = stock_mensual.apply(lambda x: 'A' if x['F_ACUMULADA'] <= 80 else 'B' if x['F_ACUMULADA'] <=90 else 'C', axis=1)
+    stock_mensual['Cat'] = stock_mensual.apply(lambda x: 'A' if x['F_ACUMULADA'] <= 80 else 'B' if x['F_ACUMULADA'] <=90 else 'C', axis=1)
 
     ### RESERVAS
     with connections['gimpromed_sql'].cursor() as cursor:
