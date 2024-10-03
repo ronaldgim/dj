48b185c9d77a480296a74c024aa27b63adc6c74c
Erik Garces <egarces@GIMPROMED.local>
2024-10-03 16:52:47 -0500

    wms -add crud orden de empaque models

--- wms/forms.py
+++ wms/forms.py
@@ -32,12 +32,48 @@ class DespachoCartonForm(forms.ModelForm):
         fields = '__all__'
         
 
-class ProductoArmadoForm(forms.ModelForm):
+class ProductoNuevoArmadoForm(forms.ModelForm):
     
     class Meta:
         model = ProductoArmado
-        fields = '__all__'
+        fields = [
+            'product_id',
+            'nombre',
+            'marca',
+            'precio_venta',
+            'unidades',
+        ]
         
+        
+        widgets = {
+            'product_id':forms.TextInput(attrs={'class':'form-control','list':'product_list', 'readonly':'true'}),
+            'nombre':forms.TextInput(attrs={'class':'form-control'}),
+            'marca':forms.TextInput(attrs={'class':'form-control'}),
+            'precio_venta':forms.NumberInput(attrs={'class':'form-control'}),
+            'unidades':forms.NumberInput(attrs={'class':'form-control'}),
+        }
+        
+
+
+class ComponenteArmadoForm(forms.ModelForm):
+    
+    class Meta:
+        model = ProductoArmado
+        fields = [
+            'product_id',
+            'nombre',
+            'marca',
+            'unidades',
+        ]
+        
+        
+        widgets = {
+            'product_id':forms.TextInput(attrs={'class':'form-control','list':'product_list'}),
+            'nombre':forms.TextInput(attrs={'class':'form-control'}),
+            'marca':forms.TextInput(attrs={'class':'form-control'}),
+            'precio_venta':forms.NumberInput(attrs={'class':'form-control'}),
+            'unidades':forms.NumberInput(attrs={'class':'form-control', 'required':'true'}),
+        }
 
 class OrdenEmpaqueForm(forms.ModelForm):
     
@@ -60,5 +96,27 @@ class OrdenEmpaqueForm(forms.ModelForm):
             'prioridad':forms.Select(attrs={'class':'form-select'}),
             'observaciones':forms.Textarea(attrs={'class':'form-control', 'rows':'2'}),
             'estado':forms.TextInput(attrs={'type':'hidden'}),
+            'usuario':forms.TextInput(attrs={'type':'hidden'}),
+        }
+
+class OrdenEmpaqueUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = OrdenEmpaque
+        fields = [
+            'ruc',
+            'cliente',
+            'bodega',
+            'prioridad',
+            'usuario',
+            'observaciones'
+        ]
+        
+        widgets = {
+            'ruc':forms.TextInput(attrs={'class':'form-control','list':'ruc_list'}),
+            'cliente':forms.TextInput(attrs={'class':'form-control', 'list':'cliente_list'}),
+            'bodega':forms.Select(attrs={'class':'form-select'}),
+            'prioridad':forms.Select(attrs={'class':'form-select'}),
+            'observaciones':forms.Textarea(attrs={'class':'form-control', 'rows':'2'}),
             'usuario':forms.TextInput(attrs={'type':'hidden'}),
         }
