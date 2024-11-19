7df79a3a47d8ea42da56a1a82195417d1a6ef255
Erik Garces <egarces@GIMPROMED.local>
2024-11-19 15:44:26 -0500

    fix dashboard completo

--- gimpromed/settings.py
+++ gimpromed/settings.py
@@ -211,15 +211,16 @@ USE_TZ = False #True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
