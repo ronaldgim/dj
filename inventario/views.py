0e2ebe5a11b27d8b34b267c30a564859d0c8ef54
Erik Garces <egarces@GIMPROMED.local>
2024-12-03 15:47:58 -0500

    inventario -stock inicio

--- inventario/views.py
+++ inventario/views.py
@@ -111,10 +111,10 @@ def stock_lote_tupla():
         location    = i.get('LOCATION').replace('/','') if '/' in i.get('LOCATION') else i.get('LOCATION')
         unidades_caja = i.get('Unidad_Empaque')
         
-        numero_cajas = 0 #''
-        unidades_sueltas = 0 #''
-        total_unidades = 0 #''
-        diferencia = 0 #''
+        numero_cajas = 0 
+        unidades_sueltas = 0 
+        total_unidades = 0 
+        diferencia = 0 
         
         observaciones = ''
         llenado = False
