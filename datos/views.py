c2314dabbe74c29e4d68bfb2a7b500a68f97ffd4
Erik Garces <egarces@GIMPROMED.local>
2025-03-27 09:28:34 -0500

    etiquetado -fix avance

--- datos/views.py
+++ datos/views.py
@@ -2142,7 +2142,9 @@ def etiquetado_avance_pedido(n_pedido):
     avance = EtiquetadoAvance.objects.filter(n_pedido=n_pedido).values()
     avance = pd.DataFrame(avance) 
     avance = avance.rename(columns={'product_id':'PRODUCT_ID'})
-    avance = avance.groupby(by=['id','n_pedido','PRODUCT_ID'])['unidades'].sum().reset_index()
+    #avance = avance.groupby(by=['id','n_pedido','PRODUCT_ID'])['unidades'].sum().reset_index()
+    #avance = avance.groupby(by=['n_pedido','PRODUCT_ID'])['unidades'].sum().reset_index()
+    #print(avance)
 
     return avance
 
