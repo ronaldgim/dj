2f6a8a3db02ad17f863060423420b80ae17cec4d
Erik Garces <egarces@GIMPROMED.local>
2025-04-24 13:52:30 -0500

    quitar correo prueba

--- wms/views.py
+++ wms/views.py
@@ -566,8 +566,8 @@ def wms_home(request):
     # api_actualizar_reservas_etiquetado()
     
     
-    if request.user.id == 2:    
-        wms_correo_picking('88601.0')
+    # if request.user.id == 2:    
+    #     wms_correo_picking('88601.0')
     
     tiempo_de_almacenamiento = kpi_tiempo_de_almacenamiento()
     capacidad_tabla = de_dataframe_a_template(kpi_capacidad()) 
