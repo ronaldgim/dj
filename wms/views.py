0e9e07754cc5a0f91633881761a83222592bf173
Erik Garces <egarces@GIMPROMED.local>
2025-12-02 10:13:11 -0500

    wms -fix finalizado 2

--- wms/views.py
+++ wms/views.py
@@ -2793,21 +2793,21 @@ def wms_estado_picking_actualizar_ajax(request):
             #         'alert':'warning'
             #     })
                 
-            if movs_total_unidades != data_reservas_total_unidades: #pick_total_unidades: 
-                return JsonResponse({
-                    # 'msg':f' ⚠ El total de items de WMS {movs_total_unidades} es diferente al total de items MBA {pick_total_unidades} !!!',
-                    'msg':f' ⚠ El total de items de WMS {movs_total_unidades} es diferente al total de items MBA {data_reservas_total_unidades} !!!',
-                    'alert':'warning'
-                })
+            # if movs_total_unidades != data_reservas_total_unidades: #pick_total_unidades: 
+            #     return JsonResponse({
+            #         # 'msg':f' ⚠ El total de items de WMS {movs_total_unidades} es diferente al total de items MBA {pick_total_unidades} !!!',
+            #         'msg':f' ⚠ El total de items de WMS {movs_total_unidades} es diferente al total de items MBA {data_reservas_total_unidades} !!!',
+            #         'alert':'warning'
+            #     })
             
-            elif not estado_picking.foto_picking:
-                return JsonResponse({
-                    'msg':' ⚠ Agrega la foto del picking !!!',
-                    'alert':'warning'
-                })
+            # elif not estado_picking.foto_picking:
+            #     return JsonResponse({
+            #         'msg':' ⚠ Agrega la foto del picking !!!',
+            #         'alert':'warning'
+            #     })
             
             # elif pick_total_unidades == movs_total_unidades:
-            elif data_reservas_total_unidades == movs_total_unidades:
+            # elif data_reservas_total_unidades == movs_total_unidades:
                 
                 estado_picking.estado = estado_post
                 estado_picking.fecha_actualizado = datetime.now()
