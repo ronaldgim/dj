debd454967a82263d5c32fdc891e3cf36b7ae1c7
Erik Garces <egarces@GIMPROMED.local>
2026-01-12 09:32:23 -0500

    fix inventario url

--- wms/views.py
+++ wms/views.py
@@ -1252,7 +1252,7 @@ def wms_inventario(request): #OK
     """ Inventario
         Suma de ingresos y egresos que dan el total de todo el inventario
     """
-    # wms_existencias_query_product_lote("10-160-24","3325224M")
+    # wms_existencias_query_product_lote("10-160-24","3325224M")    
     
     prod = productos_odbc_and_django()[['product_id','Nombre','Marca']]
     productos = pd.DataFrame(Existencias.objects.all().values('product_id'))
