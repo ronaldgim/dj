919019f7ba46527515157166a85c15daaed66668
Erik Garces <egarces@GIMPROMED.local>
2026-02-20 13:04:15 -0500

    contabilidad -add clientes excluidos de notificaciones

--- contabilidad/urls.py
+++ contabilidad/urls.py
@@ -7,8 +7,26 @@ from contabilidad import views
 urlpatterns = [
     
     path(
-        route = 'home',
-        view = views.home,
-        name = 'home'
+        route = 'lista',
+        view = views.lista_cuentas_por_cobrar,
+        name = 'lista'
+    ),
+    
+    path(
+        route = 'clientes-excluidos',
+        view = views.lista_clientes_excluidos,
+        name = 'clientes_excluidos'
+    ),
+    
+    path(
+        route = 'contabilidad_agregar_cliente_excluido',
+        view = views.contabilidad_agregar_cliente_excluido,
+        name = 'contabilidad_agregar_cliente_excluido'
+    ),
+    
+    path(
+        route = 'contabilidad_eliminar_cliente_excluido',
+        view = views.contabilidad_eliminar_cliente_excluido,
+        name = 'contabilidad_eliminar_cliente_excluido'
     ),
 ]
--- contabilidad/views.py
+++ contabilidad/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from warehouse.models import CuentasCobrar
+from django.shortcuts import render, redirect
+from warehouse.models import CuentasCobrar, Cliente
+from contabilidad.models import ClienteExcluido
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
@@ -16,4 +20,63 @@ def lista_cuentas_por_cobrar(request):
     context = {
         'cuentas_cobrar':cuentas_cobrar
     }
-    return render(request, 'contabilidad/home.html', context)
+    return render(request, 'contabilidad/lista_cuentas_cobrar.html', context)
+
+
+def lista_clientes_excluidos(request):
+    
+    clientes = Cliente.objects.using('gimpromed_sql').all()
+    
+    clientes_excluidos_list = list(
+        ClienteExcluido.objects
+        .values_list('codigo_cliente', flat=True)
+    )
+    
+    clientes_excluidos = (
+        Cliente.objects
+        .using('gimpromed_sql')
+        .filter(codigo_cliente__in=clientes_excluidos_list)
+    )
+    
+    context = {
+        'clientes': clientes,
+        'clientes_excluidos': clientes_excluidos
+    }
+    
+    return render(request, 'contabilidad/lista_clientes_excluidos.html', context)
+
+
+@require_POST
+def contabilidad_agregar_cliente_excluido(request):
+    
+    try:
+        codigo_cliente = request.POST.get('codigo_cliente')
+        
+        ClienteExcluido.objects.create(
+            codigo_cliente=codigo_cliente,
+        )
+        
+        messages.success(request, 'Agregado correctamente')
+        return redirect('clientes_excluidos')
+    
+    except Exception as e:
+        messages.error(request, str(e))
+        return redirect('clientes_excluidos')
+
+
+@require_POST
+def contabilidad_eliminar_cliente_excluido(request):
+    
+    try:
+        codigo_cliente = request.POST.get('codigo_cliente')
+        
+        ClienteExcluido.objects.get(
+            codigo_cliente=codigo_cliente,
+        ).delete()
+        
+        messages.success(request, 'Eliminado correctamente')
+        return redirect('clientes_excluidos')
+    
+    except Exception as e:
+        messages.error(request, str(e))
+        return redirect('clientes_excluidos')
